fa594a77b537cd2b42cae4100dedbd0de5f9042c
Alexander Härenstam <alehar@student.chalmers.se>
2015-02-10 16:14:22 +0100

    Fixed errors in model.

--- mobile/src/main/java/sketchagram/chalmers/com/model/RealtimePainting.java
+++ mobile/src/main/java/sketchagram/chalmers/com/model/RealtimePainting.java
@@ -1,11 +1,13 @@
 package sketchagram.chalmers.com.model;
 
+import java.util.List;
+
 /**
  * Created by Bosch on 10/02/15.
  */
 public class RealtimePainting extends Painting {
 
-    protected RealtimePainting(double timestamp) {
-        super(timestamp);
+    protected RealtimePainting(double timestamp, ADigitalPerson sender, List<ADigitalPerson> receiver ) {
+        super(timestamp, sender, receiver);
     }
 }
