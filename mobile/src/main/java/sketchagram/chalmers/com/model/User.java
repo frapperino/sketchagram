1a746b5040d930191d72b613afd4fad426c21774
Alexander Härenstam <alehar@student.chalmers.se>
2015-02-13 12:34:42 +0100

    LoginActivity
    Created the LoginActivity and added a password variable for user.

--- mobile/src/main/java/sketchagram/chalmers/com/model/User.java
+++ mobile/src/main/java/sketchagram/chalmers/com/model/User.java
@@ -4,6 +4,8 @@ package sketchagram.chalmers.com.model;
  * Created by Bosch on 10/02/15.
  */
 public class User extends ADigitalPerson {
+    String password = "password";   //TODO: replace with real password.
+    boolean requireLogin = true;
 
     public User(String name, String username, Profile profile) {
         super(name, username, profile);
