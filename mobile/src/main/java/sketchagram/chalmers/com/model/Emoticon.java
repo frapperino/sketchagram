3af70750f0c4713d43b05fea01a261ab4ca23eee
Magnus Huttu <magnushuttu@gmail.com>
2015-02-19 12:12:48 +0100

    Fragments implemented with some functions

--- mobile/src/main/java/sketchagram/chalmers/com/model/Emoticon.java
+++ mobile/src/main/java/sketchagram/chalmers/com/model/Emoticon.java
@@ -1,11 +1,14 @@
 package sketchagram.chalmers.com.model;
 
+import android.graphics.drawable.Drawable;
+
 import java.util.List;
 
 /**
  * Created by Bosch on 10/02/15.
  */
 public class Emoticon extends AMessage {
+
 
     protected Emoticon(double timestamp, ADigitalPerson sender, List<ADigitalPerson> receiver) {
         super(timestamp, sender, receiver);
@@ -14,5 +17,9 @@ public class Emoticon extends AMessage {
     @Override
     public String getMessage() {
         return ":D";
+    }
+
+    private void paint(){
+
     }
 }
--- mobile/src/main/java/sketchagram/chalmers/com/sketchagram/MainActivity.java
+++ mobile/src/main/java/sketchagram/chalmers/com/sketchagram/MainActivity.java
@@ -1,32 +1,45 @@
 package sketchagram.chalmers.com.sketchagram;
 
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import static android.preference.PreferenceManager.getDefaultSharedPreferences;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements EmoticonFragment.OnFragmentInteractionListener
+        , ItemFragment.OnFragmentInteractionListener, ConversationFragment.OnFragmentInteractionListener {
 
     private final String FILENAME = "user";
+    private EmoticonFragment emoticonFragment;
+    private ItemFragment itemFragment;
+    private ConversationFragment conversationFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         SharedPreferences pref = getSharedPreferences(FILENAME, 0);
-        Log.e("RANDOM", pref.getAll().toString());
+        emoticonFragment = new EmoticonFragment();
+        itemFragment = new ItemFragment();
+        conversationFragment = new ConversationFragment();
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        ft.add(R.id.fragmentlayout, conversationFragment);
+        ft.commit();
         String username =
                 pref.getString("username", "not found");
-        ((TextView)findViewById(R.id.text)).setText(username);
+//        ((TextView)findViewById(R.id.text)).setText(username);
     }
 
 
@@ -58,8 +71,39 @@ public class MainActivity extends ActionBarActivity {
             Intent intent = new Intent(MainActivity.this, LoginActivity.class);
             startActivity(intent);
             finish();
+        } else if (id == R.id.action_new_message) {
+
+            //Create a new fragment and replace the old fragment in layout.
+            FragmentTransaction t = getFragmentManager().beginTransaction();
+            t.replace(R.id.fragmentlayout, emoticonFragment);
+            t.commit();
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void imageClick(View view) {
+        Log.e("IMAGE", "click");
+
+        //Create a new fragment and replace the old fragment in layout.
+        FragmentTransaction t = getFragmentManager().beginTransaction();
+        t.replace(R.id.fragmentlayout, itemFragment);
+        t.commit();
+
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+        Log.e("FRAGMENT", uri.getFragment());
+    }
+
+    @Override
+    public void onFragmentInteraction(String id) {
+        Log.e("FRAGMENT", id);
+
+        //Create a new fragment and replace the old fragment in layout.
+        FragmentTransaction t = getFragmentManager().beginTransaction();
+        t.replace(R.id.fragmentlayout, conversationFragment);
+        t.commit();
     }
 }
