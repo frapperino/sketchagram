1ee8b41923401c4ca8342cc964e5cbe7b115b87b
alehar9320 <alehar@student.chalmers.se>
2015-04-17 19:14:56 +0200

    DrawingHelper.class was removed and instead implemented as an inner class of DrawingView.

--- mobile/src/main/java/sketchagram/chalmers/com/sketchagram/DrawingView.java
+++ mobile/src/main/java/sketchagram/chalmers/com/sketchagram/DrawingView.java
@@ -20,6 +20,7 @@ import android.widget.Toast;
 
 import java.io.ByteArrayOutputStream;
 import java.util.List;
+import java.util.Observable;
 import java.util.Observer;
 
 import sketchagram.chalmers.com.model.DrawMotionEvents;
@@ -275,5 +276,64 @@ public class DrawingView extends View {
 
     public void addHelperObserver(Observer observer) {
         helper.addObserver(observer);
+    }
+
+    /**
+     * Helpclass for drawing. Tracking time passed since last action on drawing.
+     * If the maximum awaiting time has passed, an action is made.
+     * Created by Alexander on 2015-03-27.
+     */
+    private class DrawingHelper extends Observable {
+        private long lastActionTime;
+        private Handler handler;
+        private boolean isRunning;
+        private Drawing drawing;
+
+        //Max nano-time allowed while awaiting input.
+        private static final long MAX_AWAIT_TIME = 2000000000;
+
+        public DrawingHelper() {
+            handler = new Handler();
+            isRunning = false;
+            drawing = new Drawing();
+        }
+
+        public void startMeasuring() {
+            if(!isRunning) {
+                isRunning = true;
+                AsyncTask asyncTask = new AsyncTask() {
+                    @Override
+                    protected Object doInBackground(Object[] params) {
+                        while(true) {
+                            if((System.nanoTime() - lastActionTime) >= MAX_AWAIT_TIME ) {
+                                handler.post(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        Toast.makeText(MyApplication.getContext(), "Sent drawing.", Toast.LENGTH_SHORT).show();
+                                        setChanged();
+                                        notifyObservers(drawing);
+                                    }
+                                });
+                                isRunning = false;
+                                return null;
+                            }
+                        }
+                    }
+                };
+                asyncTask.execute();
+            }
+        }
+
+        public void setAccessed() {
+            lastActionTime = System.nanoTime();
+        }
+
+        public void addMotion(DrawingEvent event) {
+            drawing.addMotion(event);
+        }
+
+        public Drawing getDrawing() {
+            return drawing;
+        }
     }
 }
