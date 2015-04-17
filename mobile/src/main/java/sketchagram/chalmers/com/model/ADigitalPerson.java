0b89565c4c9cc2d8ec8d438fd173c8bfd4c447af
alehar9320 <alehar@student.chalmers.se>
2015-04-17 16:07:32 +0200

    Update equals() in ADigitalPerson and moved sort() method to only be called once. Unfortunately compareTo seems to return a faulty value, strangely enough.

--- mobile/src/main/java/sketchagram/chalmers/com/model/ADigitalPerson.java
+++ mobile/src/main/java/sketchagram/chalmers/com/model/ADigitalPerson.java
@@ -29,6 +29,13 @@ public abstract class ADigitalPerson extends Observable{
 
     @Override
     public boolean equals(Object obj){
+        if(this.getClass() != obj.getClass()) {
+            return false;
+        } else if(this == obj) {
+            return true;
+        } else if(this.getUsername() == null || ((ADigitalPerson)obj).getUsername() == null) {
+            return false;
+        }
         return this.username.equals(((ADigitalPerson)obj).getUsername());
     }
     @Override
